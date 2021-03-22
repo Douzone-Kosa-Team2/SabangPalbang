8ef9ee30dc8ee546e102d19e6757a00af8ea1285
JongHyun94 <ant9406@naver.com>
2021-03-22 19:12:51 +0900

    [update] DB MAVEN Setting

---
+++ src/main/java/com/mycompany/sabangpalbang/dao/SabangDao.java
@@ -0,0 +1,8 @@
+package com.mycompany.sabangpalbang.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SabangDao {
+
+}
---
+++ src/main/java/com/mycompany/sabangpalbang/service/SabangService.java
@@ -0,0 +1,8 @@
+package com.mycompany.sabangpalbang.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SabangService {
+
+}
