eee36bf35afa7378a62e5977db592a25ec16d4b2
JongHyun94 <ant9406@naver.com>
2021-03-26 01:11:39 +0900

    [add] 팔방 디테일 xml 추가및 서비스 기능 추가

---
+++ src/main/java/com/mycompany/sabangpalbang/dto/Palbang_detail.java
@@ -0,0 +1,48 @@
+package com.mycompany.sabangpalbang.dto;
+
+public class Palbang_detail {
+	private int palbang_detailno;
+	private int palbang_id;
+	private String palbang_dimgoname;
+	private String palbang_dimgsname;
+	private String palbang_dimgtype;
+	private String palbang_explain;
+	
+	
+	public int getPalbang_detailno() {
+		return palbang_detailno;
+	}
+	public void setPalbang_detailno(int palbang_detailno) {
+		this.palbang_detailno = palbang_detailno;
+	}
+	public int getPalbang_id() {
+		return palbang_id;
+	}
+	public void setPalbang_id(int palbang_id) {
+		this.palbang_id = palbang_id;
+	}
+	public String getPalbang_dimgoname() {
+		return palbang_dimgoname;
+	}
+	public void setPalbang_dimgoname(String palbang_dimgoname) {
+		this.palbang_dimgoname = palbang_dimgoname;
+	}
+	public String getPalbang_dimgsname() {
+		return palbang_dimgsname;
+	}
+	public void setPalbang_dimgsname(String palbang_dimgsname) {
+		this.palbang_dimgsname = palbang_dimgsname;
+	}
+	public String getPalbang_dimgtype() {
+		return palbang_dimgtype;
+	}
+	public void setPalbang_dimgtype(String palbang_dimgtype) {
+		this.palbang_dimgtype = palbang_dimgtype;
+	}
+	public String getPalbang_explain() {
+		return palbang_explain;
+	}
+	public void setPalbang_explain(String palbang_explain) {
+		this.palbang_explain = palbang_explain;
+	}
+}
