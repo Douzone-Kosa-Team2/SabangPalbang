daa837dccbf87bf933d47cd95e815adc672da720
JongHyun94 <ant9406@naver.com>
2021-03-26 01:12:47 +0900

    [add] 주문 상세 xml 및 서비스 추가

---
+++ src/main/java/com/mycompany/sabangpalbang/dto/Order_detail.java
@@ -0,0 +1,28 @@
+package com.mycompany.sabangpalbang.dto;
+
+public class Order_detail {
+	private int order_detailno;
+	private int order_id;
+	private int order_productid;
+	
+	
+	public int getOrder_detailno() {
+		return order_detailno;
+	}
+	public void setOrder_detailno(int order_detailno) {
+		this.order_detailno = order_detailno;
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public int getOrder_productid() {
+		return order_productid;
+	}
+	public void setOrder_productid(int order_productid) {
+		this.order_productid = order_productid;
+	}
+
+}
---
+++ src/main/java/com/mycompany/sabangpalbang/service/OrderDetailService.java
@@ -0,0 +1,26 @@
+package com.mycompany.sabangpalbang.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mycompany.sabangpalbang.dao.OrderDetailDao;
+import com.mycompany.sabangpalbang.dto.Order_detail;
+
+@Service
+public class OrderDetailService {
+	@Autowired
+	private OrderDetailDao orderDetailDao;
+	
+	public void saveOrderDetail() {
+		orderDetailDao.insert();
+	}
+	public List<Order_detail> selectAll(){
+		List <Order_detail> list = orderDetailDao.selectAll();
+		return list;
+	}
+	public void deleteProduct(int order_detailno) {
+		orderDetailDao.deleteByOrderDetailno(order_detailno);
+	}
+}
