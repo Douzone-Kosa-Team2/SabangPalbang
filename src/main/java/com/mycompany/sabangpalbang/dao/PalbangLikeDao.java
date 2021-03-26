3f1a283b2d6ae55bfa9900dbcd904108bdb7c7cf
JongHyun94 <ant9406@naver.com>
2021-03-26 18:39:52 +0900

    [update/add] 팔방 좋아요 추가 및 멤버 수정

---
+++ src/main/java/com/mycompany/sabangpalbang/dao/PalbangLikeDao.java
@@ -0,0 +1,16 @@
+package com.mycompany.sabangpalbang.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mycompany.sabangpalbang.dto.Order_detail;
+import com.mycompany.sabangpalbang.dto.Palbang_like;
+
+@Mapper
+public interface PalbangLikeDao {
+	
+	public int insert();
+	public List<Palbang_like> selectAll();
+	public int deleteByPalbangLikeId(int palbang_likeid);
+}
---
+++ src/main/java/com/mycompany/sabangpalbang/dto/Palbang_like.java
@@ -0,0 +1,29 @@
+package com.mycompany.sabangpalbang.dto;
+
+public class Palbang_like {
+	private int palbang_likeid;
+	private int palbang_likepalbangid;
+	private int palbang_likememberid;
+	
+	
+	public int getPalbang_likeid() {
+		return palbang_likeid;
+	}
+	public void setPalbang_likeid(int palbang_likeid) {
+		this.palbang_likeid = palbang_likeid;
+	}
+	public int getPalbang_likepalbangid() {
+		return palbang_likepalbangid;
+	}
+	public void setPalbang_likepalbangid(int palbang_likepalbangid) {
+		this.palbang_likepalbangid = palbang_likepalbangid;
+	}
+	public int getPalbang_likememberid() {
+		return palbang_likememberid;
+	}
+	public void setPalbang_likememberid(int palbang_likememberid) {
+		this.palbang_likememberid = palbang_likememberid;
+	}
+	
+
+}
