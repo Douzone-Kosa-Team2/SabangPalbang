267bc8c1f6bfa4a22dc07942533708780dfb9a2b
angelatto <pudri109@gmail.com>
2021-03-26 13:59:38 +0900

    [add] OrderMain DTO & DAO & Mapper

---
+++ src/main/java/com/mycompany/sabangpalbang/dao/OrderMainDao.java
@@ -0,0 +1,16 @@
+package com.mycompany.sabangpalbang.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import com.mycompany.sabangpalbang.dto.OrderMain;
+
+
+@Mapper
+public interface OrderMainDao {
+
+	public int insert();
+	public List<OrderMain> selectAll();
+	public OrderMain selectOrderByUid();
+	public int deleteOrderByOrderId(int order_id);
+	
+}
