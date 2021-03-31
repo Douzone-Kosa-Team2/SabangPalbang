4a178ee0675a313e01a9f7dbabef9ae27c7236ca
JongHyun94 <ant9406@naver.com>
2021-03-31 14:03:32 +0900

    [update] 마이페이지 공백제거

--- src/main/java/com/mycompany/sabangpalbang/controller/MypageController.java
+++ src/main/java/com/mycompany/sabangpalbang/controller/MypageController.java
@@ -16,9 +16,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
-
 @Controller
 public class MypageController {
 	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
