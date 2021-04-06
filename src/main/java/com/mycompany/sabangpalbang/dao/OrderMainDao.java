c99075596e8305d13e9ad1698b79f9680f193d73
angelatto <pudri109@gmail.com>
2021-04-06 12:53:09 +0900

    [update] mypage_orderlist paging process using hashmap

--- src/main/java/com/mycompany/sabangpalbang/dao/OrderMainDao.java
+++ src/main/java/com/mycompany/sabangpalbang/dao/OrderMainDao.java
@@ -1,5 +1,6 @@
 package com.mycompany.sabangpalbang.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -12,8 +13,9 @@ public interface OrderMainDao {
 
 	public int insert();
 	public List<OrderMain> selectAll();
-	public List<OrderMain> selectOrderByUid(int order_memberid);
+	public List<OrderMain> selectOrderByUid(HashMap<String, Object> map);
 	public int deleteOrderByOrderId(int order_id);
 	public int insertOrder(OrderMain order);
+	public int count(int order_memberid);
 	
 }
