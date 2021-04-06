a65d8b171f2eb4455f771fffb887ec5ea8ba7daa
angelatto <pudri109@gmail.com>
2021-04-06 12:53:47 +0900

    [update] mypage_orderlist paging process

--- src/main/java/com/mycompany/sabangpalbang/controller/MypageController.java
+++ src/main/java/com/mycompany/sabangpalbang/controller/MypageController.java
@@ -1,7 +1,9 @@
 package com.mycompany.sabangpalbang.controller;
 
 import java.util.List;
+
 import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import com.mycompany.sabangpalbang.dto.Inquiry;
 import com.mycompany.sabangpalbang.dto.Member;
 import com.mycompany.sabangpalbang.dto.OrderMain;
@@ -68,12 +71,29 @@ public class MypageController {
 	}
 
 	@GetMapping("/mypage_orderlist")
-	public String mypage_orderlist(Model model, Authentication auth) {
+	public String mypage_orderlist(String pageNo, Model model, Authentication auth, HttpSession session) {
 		logger.info("mypage_orderlist 메시지");
 		
-		// db select
-		List<OrderMain> myOrderList = memberService.getOrderListById(memberService.getIdByEmail(auth.getName()));		
+		int intPageNo = 1;
+		if (pageNo == null) { // 클라이언트에서 pageNo가 넘어오지 않앗을때
+			// 세션에서 Pager를 찾고 PageNo를 설정
+			Pager pager = (Pager) session.getAttribute("mypage_order_pager");
+			if (pager != null) {
+				intPageNo = pager.getPageNo();
+			}
+		} else {
+			intPageNo = Integer.parseInt(pageNo);
+		}
 
+		// 없으면 Pager를 세션에 저장
+		int order_memberid = memberService.getIdByEmail(auth.getName());
+		int totalRows = memberService.getTotalOrderRows(order_memberid);
+		Pager pager = new Pager(6, 5, totalRows, intPageNo);
+		session.setAttribute("mypage_order_pager", pager);
+	
+		// db select
+		List<OrderMain> myOrderList = memberService.getOrderListById(pager, order_memberid);		
+		
 		logger.info("img : " +  myOrderList.get(0).getSabang_imgoname());
 		logger.info("name: " + myOrderList.get(0).getSabang_name());
 		
